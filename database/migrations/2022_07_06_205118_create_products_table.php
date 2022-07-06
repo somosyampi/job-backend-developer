cf8d34daef099ffe0aa1701a56398b9cd57789c9
Pita <felipedefarias@live.com>
2022-07-06 18:26:23 -0300

    Create products migration and model

---
+++ database/migrations/2022_07_06_205118_create_products_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->unique();
+            $table->float('price');
+            $table->text('decription');
+            $table->string('category');
+            $table->string('image_url')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};
